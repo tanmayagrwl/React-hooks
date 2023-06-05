c7ac512db0187b2a32ce97e6c1cd2f11949f5ce9
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-05 23:59:41 +0530

    conditionally useeffect

--- src/Components/HookCounterOne.jsx
+++ src/Components/HookCounterOne.jsx
@@ -3,14 +3,16 @@ import React, {useState, useEffect} from 'react'
 function HookCounterOne() {
 
     const [count, setCount] = useState(0)
-
+    const [name, setName] = useState('')
     useEffect(()=>{
         document.title = `Clicked ${count} times`
-    })
+        console.log('updating document title')
+    },[count])
 
 
   return (
     <div>
+        <input type="text" value={name} onChange={e => setName(e.target)}/>
         <button onClick={() => setCount(count +1 )}>Clicked {count} times</button>
     </div>
   )
