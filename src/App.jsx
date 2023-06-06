1e55e71d43b9bb9c31b00c4adb0745cd93092b87
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-06 21:31:38 +0530

    Data fetching using Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 
 import HookCounterOne from "./Components/HookCounterOne"
 import ClassCounterOne from "./Components/ClassCounterOne"
-import HookMouse from "./Components/HookMouse"
-import MouseContainer from "./Components/MouseContainer"
-import IntervalHookCounter from "./Components/IntervalHookCounter"
+import DataFetching from "./Components/DataFetching"
 function App() {
 
   return (
     <>
-     <IntervalHookCounter />
+     <DataFetching />
    
    
     </>
