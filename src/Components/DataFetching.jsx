af127c84af7b6acdd2f993d0f1000278d5627f3a
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-07 09:25:50 +0530

    fetching data one from a JSON

--- src/Components/DataFetching.jsx
+++ src/Components/DataFetching.jsx
@@ -4,27 +4,30 @@ import axios from 'axios'
 
 function DataFetching() {
 
-    const[posts, setPosts] = useState([])
+    const [id, setID] = useState(1)
+    const[post, setPost] = useState({}) 
 
     useEffect(()=>{
         axios
-            .get('https://jsonplaceholder.typicode.com/posts')
+            .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
             .then(res=>{
                 console.log(res)
-            setPosts(res.data)
+            setPost(res.data)
         })
         .catch(err => {
             console.log(err)
         })
-    },[])
+    },[id])
 
   return (
     <div>
-        <ul>
+        <input type="text" value={id} onChange={e=> setID(e.target.value)} />
+        <div>{post.title}</div>
+        {/* <ul>
             {
-                posts.map(post => <li key={post.id} >{post.title}</li>)
+                post.map(post => <li key={post.id} >{post.title}</li>)
             }
-        </ul>
+        </ul> */}
     </div>
   )
 }
